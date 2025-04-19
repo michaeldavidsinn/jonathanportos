1d5c54bf423fc90a55253026ee11105e165fa9cc
michaeldavidsinn <146686816+michaeldavidsinn@users.noreply.github.com>
2025-04-19 22:00:43 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ import { logo } from "../assets/images";
 const Navbar = () => {
   return (
     <header className='header font-medium'>
-      <NavLink to='/' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+      <NavLink to='/' className={({ isActive }) => isActive ? "text-black" : "text-black" }>
       Home
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
-        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+        <NavLink to='/about' className={({ isActive }) => isActive ? "text-black" : "text-black" }>
           About
         </NavLink>
       </nav>
